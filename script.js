6936027c259c7b9298fd7638d676b9422bc6ad73
Tales Costa <48179011+TalesCostta@users.noreply.github.com>
2022-09-21 17:00:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var paramChat;
+paramChat = window.location.href;
+let cronchatToken = '5203171793:AAFWMcOzQaKHbVKft3QN3ndbcNYyLlgDEIo';
+let hostURL = 'https://cronchatcronapp.cronapp.io/get-chat?token=';
+let chatbotURL = `${hostURL}${cronchatToken}&extraparam=${paramChat}`;
+let scriptHTML = document.createElement("script");
+scriptHTML.src = chatbotURL;
+scriptHTML.type = 'text/javascript'
+document.body.appendChild(scriptHTML);
