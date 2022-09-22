37bf34e1e88e2bc13837176f6473ae0f61580971
Tales Costa <48179011+TalesCostta@users.noreply.github.com>
2022-09-22 11:37:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var paramChat = window.location.href;
+console.log(paramChat);
+paramChat.replace('$pontoBot$', '.');
+console.log(paramChat);
 let cronchatToken = '5203171793:AAFWMcOzQaKHbVKft3QN3ndbcNYyLlgDEIo';
 let hostURL = 'https://cronchatcronapp.cronapp.io/get-chat?token=';
 let chatbotURL = `${hostURL}${cronchatToken}&extraparam=${paramChat}`;
-console.log(chatbotURL);
-chatbotURL.replace('$pontoBot$', '.');
-console.log(chatbotURL);
 let scriptHTML = document.createElement("script");
 scriptHTML.src = chatbotURL;
 scriptHTML.type = 'text/javascript'
